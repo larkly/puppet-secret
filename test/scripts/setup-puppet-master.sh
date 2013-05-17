03f68a9e3a1342ef50f1e999c63c884c9fe345c7
Dominik Richter <dominik.richter@gmail.com>
2013-05-17 19:53:51 +0200

    change file permissions on secrets once puppet is up and its user+group actually exist
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/scripts/setup-puppet-master.sh
+++ test/scripts/setup-puppet-master.sh
@@ -1,6 +1,7 @@
 echo "192.168.2.11    puppet-agent.local" >> /etc/hosts
 
-mkdir -p /secrets
-chown -R puppet:puppet /secrets
 
 puppet master --mkusers --autosign true
+
+mkdir -p /secrets
+chown -R puppet:puppet /secrets

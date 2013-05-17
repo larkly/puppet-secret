ad571e63c49ac16f32fa9e7d57b73394fe499adc
Dominik Richter <dominik.richter@gmail.com>
2013-05-17 13:58:35 +0200

    added fqdn lookup
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/puppet/parser/functions/secret.rb
+++ lib/puppet/parser/functions/secret.rb
@@ -1,6 +1,13 @@
 
 Puppet::Parser::Functions::newfunction(:secret, :type => :rvalue) do |vals|
-  secretname = vals[0]
+  secretid = vals[0] || 'default'
+
+  # get the callee (secrets are saved based on fqdn)
+  callee = lookupvar('fqdn')
+  if callee.to_s.empty?
+    raise Puppet::ParseError, "missing fully qualified domain name for callee of function 'secret()'. can't proceed without fqdn."
+  end
   
+  # point the client to his secret
   return "puppet:///secrets/secret"
 end

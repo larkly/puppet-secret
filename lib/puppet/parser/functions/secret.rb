a0717805194bd4931511f8c02873367a6a6b1039
Dominik Richter <dominik.richter@gmail.com>
2013-05-17 14:42:53 +0200

    added function to validate recipe input
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/puppet/parser/functions/secret.rb
+++ lib/puppet/parser/functions/secret.rb
@@ -5,14 +5,29 @@ Puppet::Parser::Functions::newfunction(:secret, :type => :rvalue) do |vals|
 
   # get the callee (secrets are saved based on fqdn)
   callee = lookupvar('fqdn')
-  if callee.to_s.empty?
-    raise Puppet::ParseError, "missing fully qualified domain name for callee of function 'secret()'. can't proceed without fqdn."
-  end
+
+  # validate all user input
+  validate callee, "FQDN"
+  validate secretid, "secret ID"
 
   retrieve_secret( callee, secretid )
   
   # point the client to his secret
   return "puppet:///secrets/secret"
+end
+
+def validate sth, name
+  if sth.to_s.empty?
+    raise Puppet::ParseError, "#{name} for callee of function 'secret()' is empty. can't proceed without it."
+  end
+
+  if (sth =~ /^[^\/]*$/).nil?
+    raise Puppet::ParseError, "#{name} for callee of function 'secret()' contains '/' (#{sth}). this is not allowed."
+  end
+
+  if not (sth =~ /^[.]*$/).nil?
+    raise Puppet::ParseError, "#{name} for callee of function 'secret()' contains only dots ('.') (#{sth}). this is not allowed."
+  end
 end
 
 def retrieve_secret callee, secretid

5ed9cf2a5b300c2c8a209adeeaa41a86409fa90c
Dominik Richter <dominik.richter@gmail.com>
2013-05-17 16:03:50 +0200

    added method to ensure the secret folder for the node calling this function exists
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/puppet/parser/functions/secret.rb
+++ lib/puppet/parser/functions/secret.rb
@@ -32,11 +32,39 @@ end
 
 def ensure_secret callee, secretid, opts = {}
   # do the secret defining action here...
-  secrets_dir = get_secrets_dir
-  f = File::join( secrets_dir, callee, secretid )
+  secrets_dir = get_secrets_dir callee
+  #f = File::join( secrets_dir, callee, secretid )
 end
 
-def get_secrets_dir
-  fileserv_conf = File.join(Puppet[:confdir], 'fileserver.conf')
-  "/secrets"
+def get_secrets_dir node, mount_point = "secrets"
+  require 'puppet/util/inifile'
+  require 'fileutils'
+
+  fileserver = Puppet::FileServing::Configuration.configuration
+  dir_obj = fileserver.find_mount mount_point, nil
+
+  # make sure the mountpoint actually exists
+  if dir_obj.nil?
+    raise Puppet::ParseError,
+      "can't find a folder mounted to '#{mount_point}'. "+
+      "make sure you have a folder configured in fileserving for mountpoint '#{mount_point}'. "+
+      "can't generate secret without a secret folder."
+  end
+
+  # get the specific folder for this callee
+  dir = dir_obj.path node
+
+  # make sure the secret folder exists
+  if not File::exists? dir
+    FileUtils::mkdir_p dir
+  end
+
+  # make sure it is a folder
+  if not File::directory? dir
+    raise Puppet::ParseError,
+      "secret path for mount point '#{mount_point}' is not a directory (#{dir}). "+
+      "can't generate secret without a secret folder."
+  end
+
+  dir
 end

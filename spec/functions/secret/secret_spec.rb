9f6c57e8e21049ed4309cb6231541b6dbc5f1444
Dominik Richter <dominik.richter@gmail.com>
2013-05-21 20:03:12 +0200

    define spec for calling secret without parameter ~> create files if necessary
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- spec/functions/secret/secret_spec.rb
+++ spec/functions/secret/secret_spec.rb
@@ -5,8 +5,33 @@ require 'puppet/file_serving/configuration'
 describe 'secret' do
   include RSpec::Puppet::Support
 
+  # since let doesn't seem to work here
+  #     let(:node) { 'spec.ops' }
+  # instead pull in the fqdn manually:
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
   it "should exist" do
     Puppet::Parser::Functions.function("secret").should == "function_secret"
+  end
+
+  describe "secret called by the client" do
+
+    # make sure to bootstrap whatever environment we need
+    before :each do
+      scope.expects(:lookupvar).with("fqdn").returns("spec.ops")
+
+      # mockup our configuration
+      # mount to puppet:///secrets  ==>  /tmp/secrets/%H
+      conf = Puppet::FileServing::Configuration.configuration
+      mount = Puppet::FileServing::Mount::File.new "secrets"
+      mount.path = "/tmp/secrets/%H"
+      conf.stubs(:mounts).returns("secrets" => mount)
+    end
+
+    it 'should be callable without parameters' do
+      scope.function_secret([]).should eq('puppet:///secrets/default')
+    end
+
   end
 
 end

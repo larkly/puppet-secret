de75973e02490af02d28236d5c2c07f202462b96
Dominik Richter <dominik.richter@gmail.com>
2013-05-21 21:05:33 +0200

    added preliminary specs to test the length of secrets of different types
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- spec/classes/secret_spec.rb
+++ spec/classes/secret_spec.rb
@@ -9,21 +9,25 @@ end
 describe Secret do
   describe '.generate_secret' do
 
-    it 'should have a default length of 128 bytes' do
+    it 'should have a default of binary with 128 bytes' do
       res = Secret::generate_secret
       res.length.should eq(128)
     end
 
     it 'should generate random binary' do
+      Secret::generate_secret({'method'=>'default','bytes'=>200}).length.should eq(200)
     end
 
     it 'should generate base64 secret' do
+      Secret::generate_secret({'method'=>'base64','bytes'=>10}).length.should eq(16)
     end
 
     it 'should generate y64 secret' do
+      Secret::generate_secret({'method'=>'y64','bytes'=>10}).length.should eq(16)
     end
 
     it 'should generate alphabet-based secret' do
+      Secret::generate_secret({'method'=>'alphabet','bytes'=>11}).length.should eq(16)
     end
 
   end

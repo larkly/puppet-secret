8174b4a84487b0ca7e82edd0705ae6eb0dcc3c2d
Dominik Richter <dominik.richter@gmail.com>
2013-06-05 09:24:10 +0200

    added a victim node definition to tests, so takover scenarios can be tested
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/manifests/nodes.pp
+++ test/manifests/nodes.pp
@@ -7,5 +7,9 @@ node "puppet-agent.local" inherits "base-system" {
   include secret-test
 }
 
+node "victim.local" inherits "base-system" {
+  include secret-test
+}
+
 node "puppet-master.local" inherits "base-system" {
 }

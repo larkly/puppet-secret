40dcafff7f3b59f3120f31c3ef65ff38b94e9333
Dominik Richter <dominik.richter@gmail.com>
2013-07-10 16:57:35 +0200

    bufixed requirements (ceph-common for ceph secret) and order for installation steps
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- test/scripts/setup-puppet-master.sh
+++ test/scripts/setup-puppet-master.sh
@@ -2,7 +2,13 @@ echo "192.168.2.11    puppet-agent.local" >> /etc/hosts
 
 cp /vagrant/fileserver.conf /etc/puppet/fileserver.conf
 
+apt-get update
+apt-get install -y ceph-common
+
 puppet master --mkusers --autosign true
 
-mkdir -p /secrets
-chown -R puppet:puppet /secrets
+for p in /secrets /secrets/shared
+do
+  mkdir -p "$p"
+  chown -R puppet:puppet "$p"
+done
